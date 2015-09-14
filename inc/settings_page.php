9414f1b5b84ea585b6271e614a90a3491fce77b8
Arthur Gareginyan <arthurgareginyan@gmail.com>
2015-09-14 06:04:28 +0300

    Update to version 1.2

--- inc/settings_page.php
+++ inc/settings_page.php
@@ -1,50 +1,64 @@
 <?php
 
+/**
+ * Prevent Direct Access
+ */
 defined('ABSPATH') or die("Restricted access!");
 
 /**
  * Render Settings Page
  *
- * @since 1.0
+ * @since 1.1
  */
-function specificfeeds_render_submenu_page() {
+function specificfeedsicon_render_submenu_page() {
 
 	// Page
 	?>
 	   <div class="wrap">
-		<h2 style="margin-bottom: 1em;"><?php _e( 'SpecificFeeds', 'specificfeeds' ); ?></h2>
+		<h2 style="text-align:center; color:cornflowerblue; margin-bottom: 1em; margin-bottom: 0;">
+				<?php _e( 'RSS Feed Icon for SpecificFeeds.com', 'specificfeedsicon' ); ?>
+				<br/>
+				<span style="font-size: 0.6em; color: #444; margin-top: 1px; margin-bottom: 20px;">
+					<?php _e( 'by <a href="http://mycyberuniverse.com/author.html" target="_blank" style="display:inline; padding:0;">Arthur "Berserkr" Gareginyan</a>', 'specificfeedsicon' ); ?>
+				<span/>
+		</h2>
 		<br/>
-		<form name="specificfeeds-form" action="options.php" method="post" enctype="multipart/form-data">
-			<?php settings_fields( 'specificfeeds_settings_group' ); ?>
+		<form name="specificfeedsicon-form" action="options.php" method="post" enctype="multipart/form-data">
+			<?php settings_fields( 'specificfeedsicon_settings_group' ); ?>
 
 			<!-- Sidebar -->
 			 <div id="templateside">
-				<?php do_action( 'specificfeeds-sidebar-top' ); ?>
+				<?php do_action( 'specificfeedsicon-sidebar-top' ); ?>
 				<p style="margin-top: 0">
-					<?php _e( 'This plugin allows you to insert rss-feed icon which connected to SpecificFeeds.com with the shortcode in any place of your site.', 'specificfeeds' ) ?>
+					<?php _e( 'This plugin allows you to easily add RSS feed icon by SpecificFeeds.com in any place on your web-site.', 'specificfeedsicon' ) ?>
 				</p>
 				<p>
-					<?php _e( '<a href="http://www.specificfeeds.com/" target="_blank" rel="nofollow" style="display:inline; padding:0;">SpecificFeeds</a> allows your visitors to receive messages from your Blog/RSS Feed by email. It\'s 100% free and also has several other benefits.', 'specificfeeds' ) ?>
+					<?php _e( '<a href="http://www.specificfeeds.com/" target="_blank" rel="nofollow" style="display:inline; padding:0;">SpecificFeeds.com</a> allows your visitors to receive messages from your Blog/RSS Feed by email. It\'s 100% free and also has several other benefits.', 'specificfeedsicon' ) ?>
 				</p>
 				<p>
-					<?php _e( 'To use, enter your personal link, choose icon and click "Save Changes". Then you can put shortcode [specificfeeds] in needed place. It\'s that simple!', 'specificfeeds' ) ?>
+					<?php _e( 'To use, enter your personal link, choose icon and click "Save Changes". Then you can put shortcode [specificfeeds-icon] in needed place. It\'s that simple!', 'specificfeedsicon' ) ?>
 				</p>
-				<?php do_action( 'specificfeeds-sidebar-bottom' ); ?>
+				<p style="margin-top:20px; border:1px solid rgb(184, 186, 184); border-radius:5px; padding:3px; text-align:center; background:rgb(234, 234, 234);">If you find it useful, consider making a donation:
+                    <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8CZE2SPQWVLMU" target="_blank" rel="nofollow">
+                          <img src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" alt="Make a donation">
+                    </a>
+                </p>
+				<?php do_action( 'specificfeedsicon-sidebar-bottom' ); ?>
 			 </div>
              <!-- END-Sidebar -->
 
              <!-- Form -->
 			 <div id="template">
-				<?php do_action( 'specificfeeds-form-top' ); ?>
+				<?php do_action( 'specificfeedsicon-form-top' ); ?>
 				<div>
-					<label for="specificfeeds_link" class="control-label" style="font-size:16px;padding-right:5px;"><?php echo __('Your SpecificFeeds Link :', 'specificfeeds'); ?></label>
-					<input type="text" class="form-control" name="specificfeeds_link" id="specificfeeds_link" placeholder="SF Link" value="<?php echo get_option('specificfeeds_link'); ?>">
+					<label for="specificfeedsicon_link" class="control-label" style="font-size:16px;padding-right:5px;"><?php echo __('Your SpecificFeeds Link :', 'specificfeedsicon'); ?></label>
+					<input type="text" class="form-control" name="specificfeedsicon_link" id="specificfeedsicon_link" placeholder="SF Link" value="<?php echo get_option('specificfeedsicon_link'); ?>">
 
 
 				</div>
-				<?php do_action( 'specificfeeds-textarea-bottom' ); ?>
+				<?php do_action( 'specificfeedsicon-textarea-bottom' ); ?>
 				<br/>
-				<p style="font-size:16px;"><?php _e( 'Which icon do you want to display?', 'specificfeeds' ); ?></p>
+				<p style="font-size:16px;"><?php _e( 'Which icon do you want to display?', 'specificfeedsicon' ); ?></p>
 				<div class="main_iconbox" style="padding-left:50px;">
 					<table width="50%" border="0" cellspacing="15" cellpadding="0">
 
@@ -52,77 +66,76 @@ function specificfeeds_render_submenu_page() {
 							<td><b>A SpecificFeeds icon...</b></td>
 							<td><b>...or an email icon:</b></td>
 						</tr>
-
 						<tr>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="first" <?php checked('first', get_option('specificfeeds_icon')); ?>>
-								<img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/first_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="first" <?php checked('first', get_option('specificfeedsicon_icon')); ?>>
+								<img src="<?php echo plugins_url( 'icons/first_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="four" <?php checked('four', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/four_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="four" <?php checked('four', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/four_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 						</tr>
 
 						<tr>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="second" <?php checked('second', get_option('specificfeeds_icon')); ?>>
-								<img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/second_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="second" <?php checked('second', get_option('specificfeedsicon_icon')); ?>>
+								<img src="<?php echo plugins_url( 'icons/second_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="five" <?php checked('five', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/five_one.png' width='48' height='48' align='absmiddle'/>
-							</td>
-						</tr>
-
-						<tr>
-							<td>
-							    <input type="radio" name="specificfeeds_icon" value="third" <?php checked('third', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/third_one.png' width='48' height='48' align='absmiddle'/>
-							</td>
-							<td>
-							    <input type="radio" name="specificfeeds_icon" value="six" <?php checked('six', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/six_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="five" <?php checked('five', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/five_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 						</tr>
 
 						<tr>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="seven" <?php checked('seven', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/seven_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="third" <?php checked('third', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/third_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="eight" <?php checked('eight', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/eight_one.png' width='48' height='48' align='absmiddle'/>
-							</td>
-						</tr>
-
-						<tr>
-							<td>
-							    <input type="radio" name="specificfeeds_icon" value="nine" <?php checked('nine', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/nine_one.png' width='48' height='48' align='absmiddle'/>
-							</td>
-							<td>
-							    <input type="radio" name="specificfeeds_icon" value="ten" <?php checked('ten', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/ten_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="six" <?php checked('six', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/six_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 						</tr>
 
 						<tr>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="eleven" <?php checked('eleven', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/eleven_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="seven" <?php checked('seven', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/seven_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="twelve" <?php checked('twelve', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/twelve_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="eight" <?php checked('eight', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/eight_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 						</tr>
 
 						<tr>
 							<td>
-							    <input type="radio" name="specificfeeds_icon" value="thirteen" <?php checked('thirteen', get_option('specificfeeds_icon')); ?>>
-							    <img src='http://www.specificfeeds.com/theme/classic/img/publisher/download/thirteen_one.png' width='48' height='48' align='absmiddle'/>
+							    <input type="radio" name="specificfeedsicon_icon" value="nine" <?php checked('nine', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/nine_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
+							</td>
+							<td>
+							    <input type="radio" name="specificfeedsicon_icon" value="ten" <?php checked('ten', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/ten_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+							    <input type="radio" name="specificfeedsicon_icon" value="eleven" <?php checked('eleven', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/eleven_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
+							</td>
+							<td>
+							    <input type="radio" name="specificfeedsicon_icon" value="twelve" <?php checked('twelve', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/twelve_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
+							</td>
+						</tr>
+
+						<tr>
+							<td>
+							    <input type="radio" name="specificfeedsicon_icon" value="thirteen" <?php checked('thirteen', get_option('specificfeedsicon_icon')); ?>>
+							    <img src="<?php echo plugins_url( 'icons/thirteen_one.png', __FILE__ ) ?>" width='48' height='48' align='absmiddle'/>
 							</td>
 							<td>
 							</td>
@@ -131,10 +144,10 @@ function specificfeeds_render_submenu_page() {
 				</div>
 				</br>
 				<div>
-					<?php submit_button( __( 'Save Changes', 'specificfeeds' ), 'primary', 'submit', true ); ?>
+					<?php submit_button( __( 'Save Changes', 'specificfeedsicon' ), 'primary', 'submit', true ); ?>
 				</div>
-				
-				<?php do_action( 'specificfeeds-form-bottom' ); ?>
+
+				<?php do_action( 'specificfeedsicon-form-bottom' ); ?>
 			 </div>
              <!-- END-Form -->
 
--- readme.txt
+++ readme.txt
@@ -1,24 +1,62 @@
-=== SpecificFeeds ===
-Contributors: arthur_gareginyan
-Tags: specificfeeds.com, specificfeeds.com, icon, rss, feed
-Donate link: 
+=== RSS Feed Icon for SpecificFeeds.com ===
+Contributors: Arthur Gareginyan
+Tags: specificfeeds.com, specificfeeds, specific feeds, icon, rss, feed, rss feed, иконка, значок, 
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8CZE2SPQWVLMU
 Requires at least: 3.9
-Tested up to: 4.0
-Stable tag: 1.0
+Tested up to: 4.3
+Stable tag: 1.1
 License: GPL3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
-An easy to use WordPress plugin that lets you EASILY add RSS-Feed icon which connected to SpecificFeeds.com with the shortcode in any place of your site.
+This plugin allows you to simply add RSS feed icon by SpecificFeeds.com in any place of your web-site.
 
 
 == Description ==
- 
+An easy to use WordPress plugin that lets you easily add RSS feed icon which connected to SpecificFeeds.com with the shortcode in any place of your web-site.
+
+[SpecificFeeds.com](http://www.specificfeeds.com) allows your visitors to receive messages from your Blog/RSS Feed by email. It's 100% free and also has several other benefits.
+
+To use, enter your personal link from SpecificFeeds.com, choose icon and click "Save Changes". Then you can put shortcode [specificfeeds-icon] in needed place. It's that simple!
+
+
+= Features =
+
+**Current features:**
+
+* Shortcode for displaying the RSS feed link.
+* 13 official icons from SpecificFeeds.com.
+* Ready for translation (.pot file included).
+
+**Coming soon:**
+
+* (Got more ideas? Tell me!)
+
+For more details please visit - [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-rss-feed-icon-for-specificfeeds-com.html)
+
+**Please Vote and Enjoy**
+
+* Your votes really make a difference! Thanks.
 
 
 == Installation ==
-Install SpecificFeeds just as you would any other WP Plugin:
-1. Upload "my-custom-functions" to the "/wp-content/plugins/" directory.
-2. Activate the plugin through the "Plugins" menu in WordPress.
+Install "RSS Feed Icon for SpecificFeeds.com" just as you would any other WP Plugin.
+
+Automatically via WordPress:
+
+1. Log into WordPress Dashboard of your website.
+2. Go to "Plugins" —> "add new plugins".
+3. Find this plugin and click install.
+4. Activate this plugin through the "Plugins" tab.
+
+Manual via FTP:
+
+1. Download a copy (zip file) of this plugin from WordPress.org.
+2. Unzip the zip file.
+3. Upload the unzipped directory to your website's plugin directory (/wp-content/plugins/).
+4. Log into WordPress Dashboard of your website.
+5. Activate this plugin through the "Plugins" tab.
+
+After installation, a "SpecificFeeds”  menu item will appear in the “Settings" section. Click on this in order to view plugin's administration page.
 
 [More help installing Plugins](http://codex.wordpress.org/Managing_Plugins#Installing_Plugins "WordPress Codex: Installing Plugins")
 
@@ -27,18 +65,64 @@ Install SpecificFeeds just as you would any other WP Plugin:
 = Will this Plugin work on my WordPress.com website? =
 Sorry, this plugin is available for use only on self-hosted (WordPress.org) websites.
 
-= Can i use this plugin to my language? =
-Yes. this plugin is translate redy. But If your language is not available you can make one. If you want to help us to translate this plugin to your language you are welcome.
+= Can I use this plugin on my language? =
+Yes. But If your language is not available then you can make one. This plugin is ready for translation. The .pot file is included and placed in "languages" folder. Many of plugin users would be delighted if you shared your translation with the community. Just send the translation files (*.po, *.mo) to me at the arthurgareginyan@gmail.com and I will include the translation within the next plugin update.
+
+= Where to report bug if found? =
+Please visit [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-rss-feed-icon-for-specificfeeds-com.html) and report.
+
+= Where to share any ideas or suggestions to make the plugin better? =
+Please visit [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-rss-feed-icon-for-specificfeeds-com.html) and share.
 
 
 == Screenshots ==
-1. Plugin’s page:
+1. Plugin’s page.
+
+
+== Other Notes ==
+**License**
+
+This plugin is licensed under the [GNU General Public License, version 3 (GPLv3)](http://www.gnu.org/licenses/gpl-3.0.html)
+and is distributed free of charge.
+Commercial licensing (e.g. for projects that can’t use an open-source license) is available upon request.
+
+**Credits**
+
+All icons are the property of the [SpecificFeeds.com](http://www.specificfeeds.com).
+
+**Support**
+
+* Did you enjoy this plugin? Please [donate to support ongoing development](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8CZE2SPQWVLMU). Your contribution would be greatly appreciated.
+* Do you have any ideas or suggestions to make the plugin better? I can’t wait to hear them! Please share! [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-rss-feed-icon-for-specificfeeds-com.html)
+
+**Please Vote and Enjoy**
+
+* Your votes really make a difference! Thanks.
+
+**Links**
+
+* [Developer Website](http://mycyberuniverse.com)
+* [Dedicated Plugin Page](http://mycyberuniverse.com/my_programs/wp-plugin-rss-feed-icon-for-specificfeeds-com.html)
+
 
 == Changelog ==
+= 1.2 =
+* specificfeeds.com icons have been optimised.
+* Minor fixes.
+= 1.1 =
+* Minor fixes.
 = 1.0 =
 * Initial release.
+= 0.2 =
+* Beta version.
+= 0.1 =
+* Alfa version.
 
 
 == Upgrade Notice ==
+= 1.2 =
+Please update to new version!
+= 1.1 =
+Please update to new version!
 = 1.0 =
-Please update to first stable relise!
+Please update to first stable release!
