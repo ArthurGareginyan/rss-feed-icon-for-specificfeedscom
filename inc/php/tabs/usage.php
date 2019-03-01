5c83008206d48f3fa195ba586cb1e46fc52ed52b
Arthur Gareginyan <arthurgareginyan@gmail.com>
2019-03-01 09:10:55 +0300

    Plugin updated to version 4.29

---
+++ inc/php/tabs/usage.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Prevent Direct Access
+ */
+defined( 'ABSPATH' ) or die( "Restricted access!" );
+
+/**
+ * Render Usage Tab Content
+ */
+?>
+    <div class="postbox">
+        <h3 class="title"><?php _e( 'Usage Instructions', $text ); ?></h3>
+        <div class="inside">
+            <p>
+                <?php
+                    printf(
+                        __( 'This plugin is based on, but not affiliated with, the %s service. The SpecificFeeds allows your visitors to receive messages from your Blog/RSS feed by email. You can access all information (such as emails and statistics) about your subscribers if you connect your feed to a SpecificFeeds account. It\'s 100&percnt; free and also has several other benefits.', $text ),
+                        '<a href="https://www.specificfeeds.com" target="_blank">SpecificFeeds.com</a>'
+                    );
+               ?>
+           </p>
+            <p><?php _e( 'To display the RSS feed icon that connected to the SpecificFeeds.com on the front end of your website, simply follow these steps:', $text ); ?></p>
+            <ol class="custom-counter">
+                <li><?php _e( 'Go to the "Settings" tab on this page.', $text ); ?></li>
+                <li><?php _e( 'Enter your personal link from the <a href="https://www.specificfeeds.com" target="_blank">SpecificFeeds.com</a>.', $text ); ?></li>
+                <li><?php _e( 'Select the desired settings.', $text ); ?></li>
+                <li><?php _e( 'Click the "Save changes" button.', $text ); ?></li>
+                <li>
+                    <?php _e( 'Now you have several methods for display the RSS feed icon (further just "button").', $text ); ?>
+                    <?php _e( 'Just choose the one that is more suitable for your case.', $text ); ?>
+                    <br><br>
+                    <p><?php _e( '<b>A)</b> For add the button inside a post from WordPress Post/Page Editor use the following shortcode:', $text ); ?></p>
+                    <p><?php highlight_string('[specificfeeds-icon]'); ?></p>
+                    <p><?php _e( '<b>B)</b> For add the button to the widget area (in sidebar, footer etc.) use the "Text" widget and add inside it the following shortcode:', $text ); ?></p>
+                    <p><?php highlight_string('[specificfeeds-icon]'); ?></p>
+                    <p><?php _e( '<b>C)</b> For add the button directly to a theme files, just add the following code to needed place (where you want to display the button) in your theme files:', $text ); ?></p>
+                    <p><?php highlight_string('<?php echo do_shortcode("[specificfeeds-icon]"); ?>'); ?></p>
+                </li>
+                <li><?php _e( 'Enjoy your fancy RSS feed icon.', $text ); ?> <?php _e( 'It\'s that simple!', $text ); ?></li>
+            </ol>
+            <p class="note">
+                <?php
+                    printf(
+                        __( 'If you want more options, then %s let us know %s and we will be happy to add them.', $text ),
+                        '<a href="https://www.spacexchimp.com/contact.html" target="_blank">',
+                        '</a>'
+                    );
+                ?>
+            </p>
+        </div>
+    </div>
+<?php
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: rss, feed, feeds, rss feed, rss feeds, button, icon, follow, rss2email, su
 Donate link: https://www.spacexchimp.com/donate.html
 Requires at least: 3.9
 Tested up to: 5.1
-Stable tag: 4.28
+Stable tag: 4.29
 License: GPL3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -172,6 +172,10 @@ Commercial licensing (e.g. for projects that can’t use an open-source license)
 
 
 == Changelog ==
+
+= 4.29 - 2019-03-01 =
+* Framework updated: The file "page.php" is divided into the following parts: "page.php", "usage.php", "faq.php", "support.php".
+* Framework updated: The files "settings.php", "usage.php", "faq.php", "support.php" are moved to the subfolder "tabs".
 
 = 4.28 - 2019-02-24 =
 * Added French translation. (Thanks to Hervé Bouzin)
