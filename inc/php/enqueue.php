82ded5a5e8edc4b3e70defa4d113a684b4fbb458
Arthur Gareginyan <arthurgareginyan@gmail.com>
2021-03-05 04:14:26 +0300

    Maintenance: Code formatting improved

--- inc/php/enqueue.php
+++ inc/php/enqueue.php
@@ -92,7 +92,6 @@ function spacexchimp_p002_load_scripts_admin( $hook ) {
 
     // Call the function that contains the dynamic CSS
     spacexchimp_p002_load_scripts_dynamic_css();
-
 }
 add_action( 'admin_enqueue_scripts', $plugin['prefix'] . '_load_scripts_admin' );
 
@@ -119,6 +118,5 @@ function spacexchimp_p002_load_scripts_frontend() {
 
     // Call the function that contains the dynamic CSS
     spacexchimp_p002_load_scripts_dynamic_css();
-
 }
 add_action( 'wp_enqueue_scripts', $plugin['prefix'] . '_load_scripts_frontend' );
